f4dad0affa1c3bd8dc9308bfc332dc35ccd57f41
kalle <kmthorsnes@gmail.com>
2018-02-28 22:22:05 +0100

    add navbar and pass feature test

--- features/step_definitions/navbar_steps.rb
+++ features/step_definitions/navbar_steps.rb
@@ -1,11 +1,11 @@
 Then("the {string} should be visible") do |selector|
-  expect(page).to have_selector :css, "##{selector}"
+  expect(page).to have_selector 'nav', id: 'navbar'
 end
 
 When("I click {string}") do |element|
   click_link_or_button element
 end
 
-Then("I should be redirected to the {string} page") do |page_name|
-  expect(page.current_path).to eq page_path_from(page_name)
+Then("I should be redirected to the landing page") do
+  expect(page.current_path).to eq root_path
 end
