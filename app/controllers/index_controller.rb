fe1ef6900eb2e332ba63436d7a844ed8158a5b72
Lara Thordardottir <lara-thordar@hotmail.com>
2018-03-02 10:14:59 +0100

    add method to get recommendations for song to index view and index controller

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -5,7 +5,11 @@ class IndexController < ApplicationController
   def search
     binding.pry
     RSpotify.authenticate(ENV['SpotifyClientID'],ENV['SpotifyClientSecret'])
-    @results = RSpotify::Track.search(params[:query])
+    results = RSpotify::Track.search(params[:query])
+    id = results.first.id
+    @recommendations = RSpotify::Recommendations.generate(seed_tracks: [id])  
+
+
     render :index
   end
 end
